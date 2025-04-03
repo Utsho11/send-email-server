02571297c337c2b38d0d942d0b3b47fff012733c
Utsho11 <utshoroy521@gmail.com>
2025-04-03 16:05:39 +0600

    remove the extra listen port

--- index.js
+++ index.js
@@ -904,9 +904,7 @@ app.get("/email-stats", async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, "0.0.0.0", () =>
   console.log(`Server running on port ${PORT}`)
 );
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
